49d71f7bf4b0ea719a3fb7f9e30d985e9043ab90
shahsaleem <ss.shahsaleem@gmail.com>
2023-08-17 16:22:50 +0500

    id converted to string

--- src/store/api/notesApi.js
+++ src/store/api/notesApi.js
@@ -24,6 +24,7 @@ const notesApi = createApi({
             method: "POST",
             body: {
               title: note,
+              id: new Date().getTime().toString(),
             },
           };
         },
